bf786a281e0e64b5c839490cfbc8629a04a8167d
DanieleUrro <danieleurrogit@gmail.com>
2025-09-08 15:26:33 +0200

    Primo commit

---
+++ calcolatrice.py
@@ -0,0 +1,28 @@
+def somma(a, b):
+    return a + b
+
+def sottrai(a, b):
+    return a - b
+
+def moltiplica(a, b):
+    return a * b
+
+def dividi(a, b):
+    return a / b
+
+print("=== Calcolatrice ===")
+print("Operazioni: +  -  *  /")
+
+operazione = input("Scegli operazione (+,-,*,/): ")
+
+a = float(input("Inserisci il primo numero: "))
+b = float(input("Inserisci il secondo numero: "))
+
+if operazione == "+":
+    print("Risultato:", somma(a, b))
+elif operazione == "-":
+    print("Risultato:", sottrai(a, b))
+elif operazione == "*":
+    print("Risultato:", moltiplica(a, b))
+elif operazione == "/":
+    print("Risultato:", dividi(a, b))
